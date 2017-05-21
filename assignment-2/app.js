e16823865d287870fecac3add216d6ce82809276
Lance French <lance@frenchathome.net>
2017-05-21 11:48:10 +0100

    Initial commit for assignment2

---
+++ assignment-2/app.js
@@ -0,0 +1,19 @@
+// Shopping List Check Off assignment
+(function() {
+'use strict;'
+
+    angular.module('ShoppingListCheckOff', [])
+        .controller('ToBuyController', ToBuyController)
+        .controller('AlreadyBoughtController', AlreadyBoughtController);
+    
+    ToBuyController.$inject = ['$scope'];
+    function ToBuyController($scope) {
+
+    }
+
+    AlreadyBoughtController.$inject = ['$scope'];
+    function AlreadyBoughtController($scope) {
+
+    }
+
+})();
