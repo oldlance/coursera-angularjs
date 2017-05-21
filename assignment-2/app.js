60c239aa3a9ad83103bedd03d3bec9073bc968dd
Lance French <lance@frenchathome.net>
2017-05-21 14:59:05 +0100

    Assignment 2: First working version

--- assignment-2/app.js
+++ assignment-2/app.js
@@ -4,16 +4,56 @@
 
     angular.module('ShoppingListCheckOff', [])
         .controller('ToBuyController', ToBuyController)
-        .controller('AlreadyBoughtController', AlreadyBoughtController);
+        .controller('AlreadyBoughtController', AlreadyBoughtController)
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
     
-    ToBuyController.$inject = ['$scope'];
-    function ToBuyController($scope) {
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController(checkOffService) {
+        var toBuy = this;        
 
+        toBuy.items = checkOffService.getToBuyItems();
+
+        toBuy.buyItem = function(itemIndex) {
+            checkOffService.buyItem(itemIndex);
+        }
     }
 
-    AlreadyBoughtController.$inject = ['$scope'];
-    function AlreadyBoughtController($scope) {
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(checkOffService) {
+        var alreadyBought = this;
+        alreadyBought.items = checkOffService.getAlreadyBoughtItems();
+    }
 
+    function ShoppingListCheckOffService() {
+        var service = this;
+
+        var toBuyItems = [
+                {item: "Chops", quantity: 6},
+                {item: "Shallots", quantity: 12 },
+                {item: "bulb of Garlic", quantity: 1 },
+                {item: "sprig of Thyme", quantity: 1},
+                {item: "small jar f Capers", quantity: 1 },
+                {item: "head of Brocolli", quantity: 1 },
+                {item: "New Potatoes", quantity: 12 },
+                {item: "bottle of White Wine", quantity: 1}
+                ];
+
+        var alreadyBoughtItems = [];
+
+        service.getToBuyItems = function() {
+            return toBuyItems;
+        }        
+
+        service.getAlreadyBoughtItems = function() {
+            return alreadyBoughtItems;
+        }        
+        
+        // Move a bought item from toBuy to alreadyBought
+        service.buyItem = function(itemIndex) {
+            var item = toBuyItems[itemIndex];
+            alreadyBoughtItems.push(item);
+            toBuyItems.splice(itemIndex, 1);
+        }
     }
 
 })();
