121fdc8bc064055e2c3feb3047a4f523b61795df
Lance French <lance@frenchathome.net>
2017-05-27 20:03:02 +0100

    Assignment 3: got filtering working; no display yet

--- assignment-3/app.js
+++ assignment-3/app.js
@@ -13,23 +13,48 @@ function NarrowItDownController(menuSearch) {
     self.searchTerm = "";
     self.found = [];
 
-    self.getMatchedMenuItems = function() {
-        
-
+    this.doSearch = function() {
+        menuSearch.getMatchedMenuItems(self.searchTerm).then(function (result){
+            self.found = result
+        }).catch(function(result){ console.log("An error occurred: ", result)}) 
     }
-    
-    console.log("this: ", self)
-
 }
 
 MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 function MenuSearchService($http, ApiBasePath) {
     var service = this;
 
-    service.getMatchedMenuItems
-
+    service.getMatchedMenuItems = function(searchTerm) {
+        var response = $http(
+            {
+                method: "GET",
+                url: (ApiBasePath + "/menu_items.json")
+            }
+        ).then(function(result) {
+            var items = result.data.menu_items;
+            var filteredItems = [];
+            var i;
+            for (i = 0; i < items.length; i++ ) {
+                if (isInString(items[i].description, searchTerm)) {
+                    console.log("match: ", items[i].description)
+                    filteredItems.push(items[i]);
+                }
+            }
+            return filteredItems
+        })
+        return response
+    }
 
 }
 
+function isInString(toSearch, term) {
+ var retval = false
+ if (toSearch !== undefined && term !== undefined) {
+   var cleanToSearch = toSearch.trim().toLowerCase()
+   var cleanTerm = term.trim().toLowerCase()
+   retval = cleanToSearch.includes(term)  
+ }
+ return retval
+}
 
 })()
